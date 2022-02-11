06ff4505624fde5eb088c1a88c06920e9042c62e
Kenneth Imade <kenneth.imade@yahoo.com>
2022-02-11 16:52:14 +0100

    Addes Validation to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+    validates :username, presence: true, uniqueness: true, length: {minimum: 5}
+    validates :password, presence: true, length: { in: 6..20 }
+    validates :email, presence: true, uniqueness: true
 end
